923ebb73de71257f0a52fa91b4d1abb4cacfd7ee
Camila <camilanachozerra@gmail.com>
2022-05-04 12:59:25 -0400

    Criação dos Rounds

---
+++ js/rounds.js
@@ -0,0 +1,269 @@
+// Round 1 NARUTO-PAIN
+function Round1NP() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Boa! Vamos para o segundo round!")
+            location.href = '../html/naruto-pain2.html'
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+// Round 2 NARUTO PAIN
+function Round2NP() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Mandou bem!")
+            location.href = "../html/naruto-pain3.html";
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+
+// Round 3 TODOS
+function Round3() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Você VENCEU!!")
+            win()
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+function win() {
+    window.location.href = "../html/win.html";
+}
+
+function gameOver() {
+    window.location.href = "../html/gameover.html";
+}
+
+// Round 1 NARUTO-OROCHIMARU
+function Round1NO() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Boa! Vamos para o segundo round!")
+            location.href = '../html/naruto-orochimaru2.html'
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+// Round 2 NARUTO-OROCHIMARU
+function Round2NO() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Mandou bem!")
+            location.href = "../html/naruto-orochimaru3.html";
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+// Round 1 KAKASHI-PAIN
+function Round1KP() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Boa! Vamos para o segundo round!")
+            location.href = '../html/kakashi-pain2.html'
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+// Round 2 KAKASHI-PAIN
+function Round2KP() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Mandou bem!")
+            location.href = "../html/kakashi-pain3.html";
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+// Round 1 KAKASHI-KAKUZU
+function Round1KK() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Boa! Vamos para o segundo round!")
+            location.href = '../html/kakashi-kakuzu2.html'
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+// Round 2 KAKASHI-KAKUZU
+function Round2KK() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Mandou bem!")
+            location.href = "../html/kakashi-kakuzu3.html";
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+// Round 1 SASUKE-DEIDARA
+function Round1SD() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Boa! Vamos para o segundo round!")
+            location.href = '../html/sasuke-deidara2.html'
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+// Round 2 SASUKE-DEIDARA
+function Round2SD() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Mandou bem!")
+            location.href = "../html/sasuke-deidara3.html";
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+// Round 1 SASUKE-ITACHI
+function Round1SI() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Boa! Vamos para o segundo round!")
+            location.href = '../html/sasuke-itachi2.html'
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
+// Round 2 SASUKE-ITACHI
+function Round2SI() {
+    while (pergunta1 != 'a' || 'b') {
+        var pergunta1 = prompt(`Qual o seu ataque?
+        Digite a letra da opção desejada: a ou b`)
+
+        if (pergunta1.toLowerCase() === 'a') {
+            alert("Seu personagem não é bom usando essas habilidades. Você perdeu!")
+            gameOver()
+            break
+        } else if (pergunta1.toLowerCase() === 'b') {
+            alert("Mandou bem!")
+            location.href = "../html/sasuke-itachi3.html";
+            break
+        } else {
+            alert('Resposta Inválida. Tente Novamente')
+        }
+    }
+}
+
